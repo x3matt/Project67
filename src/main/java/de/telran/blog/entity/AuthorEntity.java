f4e65f70689b90b3182ce34fb87b8a1a97970e34
Alex Loginov <alexander.loginov@takeaway.com>
2020-01-14 20:06:31 +0100

    #1 Technical debt: improve entities definitions

--- src/main/java/de/telran/blog/entity/AuthorEntity.java
+++ src/main/java/de/telran/blog/entity/AuthorEntity.java
@@ -2,16 +2,20 @@ package de.telran.blog.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
+@Table(name="author")
 @Entity
 @Data
 public class AuthorEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 }
